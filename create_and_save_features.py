bae3a06b9bc6a4bbd340c91f8bca2807a7f808bd
Tomislav Ćosić <tc52658@fer.hr>
2024-04-14 18:56:35 +0200

    Added code to create and save features so they don't have to be calculated every run.

---
+++ create_and_save_features.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import torch
+import torchvision
+
+from linear_evaluation import get_features
+from simclr import SimCLR
+from simclr.modules import get_resnet
+from simclr.modules.transformations import TransformsSimCLR
+from utils import yaml_config_hook
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="SimCLR")
+    config = yaml_config_hook("./config/config.yaml")
+    for k, v in config.items():
+        parser.add_argument(f"--{k}", default=v, type=type(v))
+
+    args = parser.parse_args()
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.dataset == "STL10":
+        train_dataset = torchvision.datasets.STL10(
+            args.dataset_dir,
+            split="train",
+            download=True,
+            transform=TransformsSimCLR(size=args.image_size).test_transform,
+        )
+        test_dataset = torchvision.datasets.STL10(
+            args.dataset_dir,
+            split="test",
+            download=True,
+            transform=TransformsSimCLR(size=args.image_size).test_transform,
+        )
+    elif args.dataset == "CIFAR10":
+        train_dataset = torchvision.datasets.CIFAR10(
+            args.dataset_dir,
+            train=True,
+            download=True,
+            transform=TransformsSimCLR(size=args.image_size).test_transform,
+        )
+        test_dataset = torchvision.datasets.CIFAR10(
+            args.dataset_dir,
+            train=False,
+            download=True,
+            transform=TransformsSimCLR(size=args.image_size).test_transform,
+        )
+    else:
+        raise NotImplementedError
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.simple_nf_batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=args.workers,
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=args.simple_nf_batch_size,
+        shuffle=False,
+        drop_last=True,
+        num_workers=args.workers,
+    )
+
+    encoder = get_resnet(args.resnet, pretrained=False)
+    n_features = encoder.fc.in_features  # get dimensions of fc layer
+
+    # load pre-trained model from checkpoint
+    simclr_model = SimCLR(encoder, args.projection_dim, n_features)
+    model_fp = os.path.join(args.model_path, "checkpoint_{}.tar".format(args.epoch_num))
+    simclr_model.load_state_dict(torch.load(model_fp, map_location=args.device.type))
+    simclr_model = simclr_model.to(args.device)
+    simclr_model.eval()
+
+    (train_X, train_y, test_X, test_y) = get_features(simclr_model, train_loader, test_loader, args.device)
+    torch.save(train_X, os.path.join(args.feature_save_path, "train_X.pt"))
+    torch.save(train_y, os.path.join(args.feature_save_path, "train_y.pt"))
+    torch.save(test_X, os.path.join(args.feature_save_path, "test_X.pt"))
+    torch.save(test_y, os.path.join(args.feature_save_path, "test_y.pt"))
